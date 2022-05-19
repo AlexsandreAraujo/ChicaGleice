96d6001d5634d607931d61538c307e604a67206d
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-05-18 23:42:57 -0300

    ajustando configs de deploy

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,13 +1,18 @@
 import { DataSource } from 'typeorm';
 import ProdutosGabiq from '../models/ProdutosGabiq';
+import 'dotenv/config';
 
 const AppDataSource = new DataSource({
     type: 'postgres',
-    host: 'localhost',
-    username: 'postgres',
-    password: 'alexla@1',
+    // host: 'localhost',
+    // username: 'chicaroot',
+    // password: 'ac9cd30b9f33c74af49dde8bd445d5a8',
+    // database: 'ChicaGleice',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     port: 5432,
-    database: 'ChicaGleice',
     migrations: ['./src/database/migrations/**/*{.ts,.js}'],
     entities: [ProdutosGabiq],
 });
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import app from './app';
 import AppDataSource from './database/db';
+import 'dotenv/config';
 
-app.listen(3333, async () => {
+app.listen(process.env.PORT || 3333, async () => {
     await AppDataSource.initialize();
     console.log('🚀 Server started on port 3333!');
 });
