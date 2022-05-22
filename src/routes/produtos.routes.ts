41a7a4fa2e2f3da8f1280c9e5cc092e89a96c3ac
Alexsandre Araujo <alexsandre.lima.araujo@outlook.com>
2022-05-21 23:00:06 -0300

    adicionado campos para pesquisa GET

--- src/routes/produtos.routes.ts
+++ src/routes/produtos.routes.ts
@@ -1,12 +1,31 @@
 import { Router } from 'express';
+import { Equal, MoreThanOrEqual, ILike } from 'typeorm';
 import ProdutosGabiq from '../models/ProdutosGabiq';
 import ProdutosGabiqService from '../services/produtos/gabiq';
 
 const produtosRouter = Router();
 
 produtosRouter.get('/', async (request, response) => {
-    const produtos = await ProdutosGabiq.find();
-
+    const {
+        category,
+        name,
+        available,
+        option0,
+        option1,
+        // option2,
+        preco,
+        // stock,
+    } = request.body;
+    const produtos = await ProdutosGabiq.findBy({
+        category: ILike(`%${category}%`),
+        name: ILike(`%${name}%`),
+        available: Equal(available),
+        option0: ILike(`%${option0}%`),
+        option1: ILike(`%${option1}%`),
+        // option2: Like(`%${option2}%`),
+        preco: MoreThanOrEqual(preco),
+        // stock: MoreThanOrEqual(stock),
+    });
     return response.json(produtos);
 });
 
